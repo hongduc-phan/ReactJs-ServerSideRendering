ddb762ddf7c57df30de76d7d8443fa39f6029a90
Duc Phan <phduc.phanhongduc@gmail.com>
2017-10-16 16:06:25 +0300

    config webpack to pack JSX and Js files

--- app/app.js
+++
@@ -1,8 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-ReactDOM.render(
-  React.createElement('a',{href: 'https://www.facebook.com/HarryFankJefferson'}, 'PhanHongDuc3'),
-  document.getElementById('root')
-
-)
---
+++ app/app.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <a href: "https://www.facebook.com/HarryFankJefferson"> PhanHongDuc4</a>,
+  document.getElementById('root')
+
+)
